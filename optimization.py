d87650f19726bb34a557c37e43c54bda182245c8
Justin Pounders <justin_pounders@uml.edu>
2016-10-24 16:13:18 -0400

    Added L2 reguralization

--- optimization.py
+++ optimization.py
@@ -1,10 +1,14 @@
 import numpy as np
+import warnings
 
-def gradient_descent(grad_func, alpha, theta, max_iters=500):
+def gradient_descent(grad_func, alpha, theta, max_iters=500, tol=0.001):
     for iter in range(max_iters):
         theta_old = theta
         theta = theta - alpha*grad_func(theta)
         err = np.abs(theta_old - theta)
-        if np.max(err/theta) < 0.001: break
+        if np.max(err/theta) < tol: break
 
-    return theta
+    if iter==max_iters-1:
+        warnings.warn('Gradient descent did not converge.')
+
+    return theta,iter
--- regression.py
+++ regression.py
@@ -16,16 +16,37 @@ class LinearRegression:
         
         return np.dot(X,theta)
     
-    def cost(self,X,theta,y):
+    def cost(self,X,theta,y,lmbda=0.0):
         """Calculate the cost/objective function given observations X, linear weights theta, and target y."""
-        return (np.dot(X,theta) - y)**2/(2.0*X.shape[0])
+        m = X.shape[0]
+        return (np.dot(X,theta) - y)**2/(2.0*m) + self.reg_func(theta,lmbda/m)
 
-    def cost_grad(self,X,theta,y):
+    def cost_grad(self,X,theta,y,lmbda=0.0):
         """Calculate the gradient of the cost/objective function at theta."""
-        return np.dot(X.T, np.dot(X,theta) - y)/X.shape[0]
+        m = X.shape[0]
+        return np.dot(X.T, np.dot(X,theta) - y)/m + self.reg_grad(theta,lmbda/m)
 
-    def train(self,X,y):
+    def reg_func(self,theta,coeff):
+        """Calculate the regularization term (ridge)."""
+        return coeff*(np.dot(theta,theta) - theta[0]**2)
+
+    def reg_grad(self,theta,coeff):
+        """Calculate the gradient of the regularization term (ridge)."""
+        grad = theta.copy()
+        grad[0] = 0.0
+        return coeff*grad
+
+    def train(self,X,y,lmbda=0.0,tol=0.0001,max_iters=500):
         """Train on data X with target y."""
-        def grad_func(theta): return self.cost_grad(X,theta,y)
-        self.theta_trained = opt.gradient_descent(grad_func, 0.1, np.zeros(X.shape[1]))
+        def grad_func(theta): return self.cost_grad(X,theta,y,lmbda)
+        self.theta_trained,iter = opt.gradient_descent(grad_func,
+                                                       0.1,
+                                                       np.zeros(X.shape[1]),
+                                                       max_iters,
+                                                       tol)
+        return self.theta_trained
+
+    def solve_normal_eqns(self,X,y,lmbda=0.0):
+        """Solve the normal equations to get the linear regression solution."""
+        self.theta_trained = np.linalg.solve(np.dot(X.T,X), np.dot(X.T,y))
         return self.theta_trained
