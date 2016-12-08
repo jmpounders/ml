8cb5291c36253065252b42224205842d88f47632
Justin Pounders <justin_pounders@uml.edu>
2016-12-08 15:42:12 -0500

    return 0 if not converged

--- optimization.jl
+++ optimization.jl
@@ -2,7 +2,7 @@ module Optimization
 
 export gradient_descent
 
-function gradient_descent(grad_func, alpha, theta::Array{Float64,1}, max_iters=500, tol=1.0e-4)
+function gradient_descent(grad_func, alpha, theta::Array{Float64,1}, max_iters=1000, tol=1.0e-4)
     converged = false
     for iter = 1:max_iters
         theta_old = theta
@@ -16,6 +16,7 @@ function gradient_descent(grad_func, alpha, theta::Array{Float64,1}, max_iters=5
 
     if !converged
         println("Gradient descent did not converge.")
+        return theta*0
     end
     theta
 end
